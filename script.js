424b5f0a75189c4c6eccea790f6af381bb79d0a7
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-08-04 10:45:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,66 @@
-$(document).ready(function() {
- 
-  $("#generateBtn").on("click", function() {
+var inputEl = document.getElementById('qr-input');
+var canvasEl = document.getElementById('qr-canvas');
+var downloadBtn = document.getElementById('download-btn');
+var printBtn = document.getElementById('print-btn');
+var titleEl = document.getElementById('title'); 
 
-    $("#qrCodes").empty();
-
-
-    $(".qrInput").each(function(index) {
-
-      const inputValue = $(this).val();
-
-      if (inputValue.trim() !== '') {
-
-        const qrContainerId = `qrCode_${index + 1}`;
-        const qrContainer = $(`<div id="${qrContainerId}" class="qrCodeContainer"></div>`);
-        $("#qrCodes").append(qrContainer);
-
-        const qrcode = new QRCode(document.getElementById(qrContainerId), {
-          text: inputValue,
-          width: 128,
-          height: 128,
-        });
-      }
+function updateQRCode() {
+  if (inputEl.value.trim() === '') {
+    canvasEl.width = 0;
+    titleEl.textContent = '';
+    downloadBtn.disabled = true;
+    printBtn.disabled = true;
+  } else {
+    var qr = new QRious({
+      element: canvasEl,
+      value: inputEl.value,
+      size: 100,
+      level: 'H',
+      background: '#fff',
+      foreground: '#333'
     });
-  });
+    titleEl.textContent = inputEl.value;
+    downloadBtn.disabled = false;
+    printBtn.disabled = false;
+  }
+}
+
+inputEl.addEventListener('input', updateQRCode);
+
+downloadBtn.addEventListener('click', function() {
+  var canvas = document.createElement('canvas');
+  canvas.width = canvasEl.width;
+  canvas.height = canvasEl.height + 20; // increased to accommodate the text
+  var ctx = canvas.getContext('2d');
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.font = '20px Arial';
+  ctx.fillStyle = '#333';
+  ctx.textAlign = 'center';
+  ctx.fillText(inputEl.value, canvas.width/2, 15); // position the text above the QR code
+  ctx.drawImage(canvasEl, 0, 20); // position the QR code below the text
+  var link = document.createElement('a');
+  link.download = inputEl.value + '.png';
+  link.href = canvas.toDataURL();
+  link.click();
 });
+
+
+printBtn.addEventListener('click', function() {
+  var canvas = document.createElement('canvas');
+  canvas.width = canvasEl.width;
+  canvas.height = canvasEl.height + 20; // increased to accommodate the text
+  var ctx = canvas.getContext('2d');
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = '#333';
+  ctx.font = '20px Arial';
+  ctx.textAlign = 'center';
+  ctx.fillText(inputEl.value, canvas.width/2, 15); // position the text above the QR code
+  ctx.drawImage(canvasEl, 0, 20) // position the QR code below the text
+  var printWindow = window.open();
+  printWindow.document.write('<div><img src="' + canvas.toDataURL() + '" onload="window.print()"></div>');
+  printWindow.document.close();
+});
+
+updateQRCode();
